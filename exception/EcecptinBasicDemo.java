9cad3fb31cfa4b51e0bf97ed4607c02a6a6563ad
Yashvant Giri <ldhar3359@gmail.com>
2026-03-24 10:24:24 +0530

    finaly done

--- exception/EcecptinBasicDemo.java
+++ exception/EcecptinBasicDemo.java
@@ -1,15 +1,22 @@
+//NumberFormatException-  parseInt() method is used to convert a string into an integer.
 package exception;
 
 public class EcecptinBasicDemo {
     public static void main(String[] args) {
-        String a="abc";
-        int num = Integer.parseInt(a);
-        System.out.println(num);
-    }
-    catch (NumberFormatException e) {
-        System.out.println("Error: Invalid number format.");
-    }
-   
-}
+//         try {
+//         String a="8";
+//         int num = Integer.parseInt(a);
+//         System.out.println(num);
+//     }
+//     catch(NumberFormatException e) {
+//         System.out.println("Error: Invalid number format.");
+//     }
+
+// }
+// }
+    
+   //2. Double.parseDouble()
+ //String → double (decimal number)
+
 
 
