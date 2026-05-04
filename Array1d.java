15859e3d978768f9839177ddd93fbe1044c44e98
Yashvant Giri <ldhar3359@gmail.com>
2026-05-04 23:09:27 +0530

    ok

--- Array1d.java
+++ Array1d.java
@@ -11,3 +11,12 @@ for (int i=0; i < a.length; i++)
 }
 }
 }
+
+
+
+//public class Main {
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    System.out.println(cars[0]);
+  }
+}
